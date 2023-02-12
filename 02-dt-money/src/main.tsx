1dcbdbf5ef9ca9de50c08bac44b078105fb557db
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-12 13:02:56 +0200

    feat(dtmoney): setup axios and miragejs

--- 02-dt-money/src/main.tsx
+++ 02-dt-money/src/main.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createServer } from "miragejs";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createServer({
+    routes() {
+        this.namespace = "/api";
+        this.urlPrefix = "http://localhost:3000";
+        this.get("transactions", () => [
+            {
+                id: 1,
+                title: "Transaction 1",
+                amount: 400,
+                type: "deposit",
+                category: "food",
+                createdAt: new Date(),
+            },
+        ]);
+    },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
