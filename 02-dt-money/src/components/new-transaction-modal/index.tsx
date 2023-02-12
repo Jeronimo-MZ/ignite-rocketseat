4fbbc212941ceec9834b1781c92faedd1f0ee317
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-13 01:10:47 +0200

    feat(dtmoney): add new transaction modal close button

--- 02-dt-money/src/components/new-transaction-modal/index.tsx
+++ 02-dt-money/src/components/new-transaction-modal/index.tsx
@@ -1,5 +1,6 @@
 import ReactModal from "react-modal";
 import { Container } from "./styles";
+import closeImage from "../../assets/close.svg";
 
 ReactModal.setAppElement("#root");
 
@@ -19,6 +20,13 @@ export function NewTransactionModal({
             overlayClassName="react-modal-overlay"
             className="react-modal-content"
         >
+            <button
+                type="button"
+                onClick={onRequestClose}
+                className="react-modal-close"
+            >
+                <img src={closeImage} alt="Fechar modal" />
+            </button>
             <Container>
                 <h2>Cadastrar Transação</h2>
                 <input type="text" placeholder="Título" />
