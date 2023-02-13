cfb565b570f082403c3cfc5c175bd9fe97f04502
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-13 20:33:55 +0200

    feat(ignews): add faunadb

--- 03-ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,5 @@
+import { fauna } from "@/service/fauna";
+import { query as q } from "faunadb";
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 
@@ -8,9 +10,37 @@ export default NextAuth({
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
             authorization: {
                 params: {
-                    scope: "read:user",
+                    scope: "read:user user:email",
                 },
             },
         }),
     ],
+    callbacks: {
+        async signIn({ user: { email } }) {
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index("user_by_email"),
+                                    q.Casefold(email as string)
+                                )
+                            )
+                        ),
+                        q.Create(q.Collection("users"), { data: { email } }),
+                        q.Get(
+                            q.Match(
+                                q.Index("user_by_email"),
+                                q.Casefold(email as string)
+                            )
+                        )
+                    )
+                );
+                return true;
+            } catch (error) {
+                return false;
+            }
+        },
+    },
 });
