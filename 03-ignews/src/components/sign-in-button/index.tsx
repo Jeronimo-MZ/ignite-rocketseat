4684e82efff9741aec3a95628957dbc58a458d49
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-13 19:47:31 +0200

    feat(ignews): add next-auth

--- 03-ignews/src/components/sign-in-button/index.tsx
+++ 03-ignews/src/components/sign-in-button/index.tsx
@@ -1,18 +1,21 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signIn, signOut, useSession } from "next-auth/react";
+
 export function SignInButton() {
-    const isUserLoggedIn = false;
+    const { data, status } = useSession();
+    const isUserLoggedIn = status === "authenticated";
     if (isUserLoggedIn) {
         return (
-            <button className={styles.container}>
+            <button className={styles.container} onClick={() => signOut()}>
                 <FaGithub color="#04d361" /> Jerónimo Matavel
                 <FiX />
             </button>
         );
     }
     return (
-        <button className={styles.container}>
+        <button className={styles.container} onClick={() => signIn()}>
             <FaGithub color="#eba417" /> Sign in with Github
         </button>
     );
--- 03-ignews/src/pages/_app.tsx
+++ 03-ignews/src/pages/_app.tsx
@@ -2,12 +2,13 @@ import type { AppProps } from "next/app";
 
 import "@/styles/globals.scss";
 import { Header } from "@/components/header";
+import { SessionProvider as NextAuthProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <>
+        <NextAuthProvider session={pageProps.session}>
             <Header />
             <Component {...pageProps} />
-        </>
+        </NextAuthProvider>
     );
 }
