5cf8f3121fa1787f7fba84b3507c79f65beb3796
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-15 07:20:04 +0200

    feat(dashgo): add sidebar

--- 04-dashgo/src/pages/dashboard.tsx
+++ 04-dashgo/src/pages/dashboard.tsx
@@ -1,9 +1,14 @@
 import { Header } from "@/components/header";
+import { Sidebar } from "@/components/sidebar";
+import { Flex } from "@chakra-ui/react";
 
 export default function Dashboard() {
     return (
-        <>
+        <Flex direction="column" h="100vh">
             <Header />
-        </>
+            <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+                <Sidebar />
+            </Flex>
+        </Flex>
     );
 }
