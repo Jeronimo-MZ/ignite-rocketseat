8f4f9960b500d79b28a553bc60ff43b1c6206646
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-15 05:43:58 +0200

    feat(dashgo): add auth page

--- 04-dashgo/src/pages/index.tsx
+++ 04-dashgo/src/pages/index.tsx
@@ -1,3 +1,11 @@
+import {
+    Button,
+    Flex,
+    FormControl,
+    FormLabel,
+    Input,
+    Stack,
+} from "@chakra-ui/react";
 import Head from "next/head";
 
 export default function Home() {
@@ -11,7 +19,47 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main>Hello World</main>
+            <Flex align="center" justify="center" w="100vw" minH="100vh">
+                <Flex
+                    as="form"
+                    flexDir="column"
+                    w="100%"
+                    maxW={360}
+                    bg="gray.800"
+                    p="8"
+                    rounded="md"
+                >
+                    <Stack spacing="4">
+                        <FormControl>
+                            <FormLabel>E-mail</FormLabel>
+                            <Input
+                                type="email"
+                                focusBorderColor="pink.500"
+                                bg="gray.900"
+                                variant="filled"
+                                _hover={{ bg: "gray.900" }}
+                                size="lg"
+                                placeholder="john@example.com"
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Password</FormLabel>
+                            <Input
+                                type="password"
+                                focusBorderColor="pink.500"
+                                bg="gray.900"
+                                variant="filled"
+                                _hover={{ bg: "gray.900" }}
+                                size="lg"
+                                placeholder="********"
+                            />
+                        </FormControl>
+                    </Stack>
+                    <Button type="submit" mt="6" colorScheme="pink">
+                        Entrar
+                    </Button>
+                </Flex>
+            </Flex>
         </>
     );
 }
