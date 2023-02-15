353e3e8964d3524f73444c0d13f34ba9a1298201
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-15 07:05:32 +0200

    feat(dashgo): add icons and avatar to header

--- 04-dashgo/src/components/header.tsx
+++ 04-dashgo/src/components/header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Input, Text, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from "@chakra-ui/react";
+import {
+    RiNotificationLine,
+    RiSearchLine,
+    RiUserAddLine,
+} from "react-icons/ri";
 export function Header() {
     return (
         <Flex
@@ -40,6 +44,33 @@ export function Header() {
                     _placeholder={{ color: "gray.400" }}
                 />
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+            <Flex align="center" ml="auto">
+                <HStack
+                    spacing="8"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.600"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+                <Flex align="center">
+                    <Box mr="4" textAlign="right">
+                        <Text>Jerónimo Matavel</Text>
+                        <Text color="gray.300" fontSize="small">
+                            mataveljeronimo@gmail.com
+                        </Text>
+                    </Box>
+                    <Avatar
+                        size="md"
+                        name="Jerónimo Matavel"
+                        src="https://github.com/jeronimo-mz.png"
+                    />
+                </Flex>
             </Flex>
         </Flex>
     );
