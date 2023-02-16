dbd4fe2d85e36aa871a041c492eb818354a456c3
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-16 18:25:26 +0200

    feat(dashgo): render users from api

--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -24,15 +24,39 @@ import NextLink from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { useQuery } from "@tanstack/react-query";
 
+type User = {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: string;
+};
+
 export default function UsersList() {
     const isWideVersion = useBreakpointValue({ base: false, lg: true });
     const { data, error, isLoading } = useQuery({
         queryKey: ["users"],
         queryFn: async () => {
             const response = await fetch("http://localhost:3000/api/users");
-            return response.json();
+            const data = await response.json();
+            const users = data.users.map((user: User) => {
+                return {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    createdAt: new Date(user.createdAt).toLocaleDateString(
+                        "pt-BR",
+                        {
+                            day: "2-digit",
+                            month: "long",
+                            year: "numeric",
+                        }
+                    ),
+                };
+            });
+            return users;
         },
     });
+
     return (
         <>
             <Head>
@@ -90,51 +114,53 @@ export default function UsersList() {
                                             </Tr>
                                         </Thead>
                                         <Tbody>
-                                            <Tr>
-                                                <Td
-                                                    px={["4", "4", "6"]}
-                                                    color="gray.300"
-                                                    width="8"
-                                                >
-                                                    <Checkbox colorScheme="pink" />
-                                                </Td>
-                                                <Td>
-                                                    <Box>
-                                                        <Text fontWeight="bold">
-                                                            Jerónimo Matavel
-                                                        </Text>
-                                                        <Text
-                                                            fontSize="sm"
-                                                            color="gray.300"
-                                                        >
-                                                            mataveljeronimo@gmail.com
-                                                        </Text>
-                                                    </Box>
-                                                </Td>
-                                                {isWideVersion && (
-                                                    <Td>
-                                                        12 de Fevereiro de 2023
+                                            {data.map((user: User) => (
+                                                <Tr key={user.id}>
+                                                    <Td
+                                                        px={["4", "4", "6"]}
+                                                        color="gray.300"
+                                                        width="8"
+                                                    >
+                                                        <Checkbox colorScheme="pink" />
                                                     </Td>
-                                                )}
-                                                {isWideVersion && (
                                                     <Td>
-                                                        <Button
-                                                            size="sm"
-                                                            fontSize="sm"
-                                                            colorScheme="purple"
-                                                            leftIcon={
-                                                                <Icon
-                                                                    as={
-                                                                        RiPencilLine
-                                                                    }
-                                                                />
-                                                            }
-                                                        >
-                                                            Editar
-                                                        </Button>
+                                                        <Box>
+                                                            <Text fontWeight="bold">
+                                                                {user.name}
+                                                            </Text>
+                                                            <Text
+                                                                fontSize="sm"
+                                                                color="gray.300"
+                                                            >
+                                                                {user.email}
+                                                            </Text>
+                                                        </Box>
                                                     </Td>
-                                                )}
-                                            </Tr>
+                                                    {isWideVersion && (
+                                                        <Td>
+                                                            {user.createdAt}
+                                                        </Td>
+                                                    )}
+                                                    {isWideVersion && (
+                                                        <Td>
+                                                            <Button
+                                                                size="sm"
+                                                                fontSize="sm"
+                                                                colorScheme="purple"
+                                                                leftIcon={
+                                                                    <Icon
+                                                                        as={
+                                                                            RiPencilLine
+                                                                        }
+                                                                    />
+                                                                }
+                                                            >
+                                                                Editar
+                                                            </Button>
+                                                        </Td>
+                                                    )}
+                                                </Tr>
+                                            ))}
                                         </Tbody>
                                     </Table>
                                 </TableContainer>
