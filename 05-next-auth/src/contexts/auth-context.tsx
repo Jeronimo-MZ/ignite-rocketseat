691b5394f91a9a18ec9c6dbee1c69aef4034615b
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-17 01:17:55 +0200

    feat(next-auth): save tokens in cookies

--- 05-next-auth/src/contexts/auth-context.tsx
+++ 05-next-auth/src/contexts/auth-context.tsx
@@ -1,5 +1,7 @@
 import { api } from "@/services/api";
+import { useRouter } from "next/router";
 import { createContext, PropsWithChildren, useContext, useState } from "react";
+import { setCookie } from "nookies";
 
 type SignInCredentials = {
     email: string;
@@ -8,23 +10,44 @@ type SignInCredentials = {
 type AuthContextData = {
     signIn(credentials: SignInCredentials): Promise<void>;
     isAuthenticated: boolean;
+    user: User | null;
 };
 
 const AuthContext = createContext({} as AuthContextData);
 
+type User = {
+    email: string;
+    permissions: string[];
+    roles: string[];
+};
 export const AuthProvider = ({ children }: PropsWithChildren) => {
-    const [isAuthenticated] = useState(false);
+    const [user, setUser] = useState<User | null>(null);
+    const isAuthenticated = !!user;
+    const router = useRouter();
 
     const signIn = async ({ email, password }: SignInCredentials) => {
         try {
-            const response = await api.post("/sessions", { email, password });
-            console.log(response.data);
+            const response = await api.post("/sessions", {
+                email,
+                password,
+            });
+            const { permissions, roles, token, refreshToken } = response.data;
+            setUser({ email, permissions, roles });
+            setCookie(null, "next-auth.token", token, {
+                maxAge: 60 * 60 * 24 * 30, //30 days
+                path: "/",
+            });
+            setCookie(null, "next-auth.refresh-token", refreshToken, {
+                maxAge: 60 * 60 * 24 * 30,
+                path: "/",
+            });
+            router.push("/dashboard");
         } catch (error) {
             console.log(error);
         }
     };
     return (
-        <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+        <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
             {children}
         </AuthContext.Provider>
     );
