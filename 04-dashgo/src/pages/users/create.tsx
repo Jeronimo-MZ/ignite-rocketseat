ce55aff02e7e326097675f0e0485afdbe4dc1ced
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-16 15:55:10 +0200

    feat(dashgo): responsive User form

--- 04-dashgo/src/pages/users/create.tsx
+++ 04-dashgo/src/pages/users/create.tsx
@@ -24,15 +24,15 @@ export default function CreateUser() {
                 <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
                     <Sidebar />
 
-                    <Box flex="1" rounded="md" bg="gray.800" p="8">
+                    <Box flex="1" rounded="md" bg="gray.800" p={["6", "8"]}>
                         <Heading size="lg" fontWeight="normal">
                             Criar Usuário
                         </Heading>
                         <Divider my="6" borderColor="gray.700" />
-                        <VStack spacing="8">
+                        <VStack spacing={["4", "8"]}>
                             <SimpleGrid
                                 minChildWidth="240px"
-                                spacing="8"
+                                spacing={["4", "8"]}
                                 width="100%"
                             >
                                 <Input name="name" label="Nome Completo" />
@@ -45,7 +45,7 @@ export default function CreateUser() {
 
                             <SimpleGrid
                                 minChildWidth="240px"
-                                spacing="8"
+                                spacing={["4", "8"]}
                                 width="100%"
                             >
                                 <Input
