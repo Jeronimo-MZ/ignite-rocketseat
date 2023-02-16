88412dee1c18e953ac47bb9649511f0ffb436f05
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-02-16 16:47:04 +0200

    feat(dashgo): add active state to nav link

--- 04-dashgo/src/components/sidebar/nav-link.tsx
+++ 04-dashgo/src/components/sidebar/nav-link.tsx
@@ -5,20 +5,35 @@ import {
     Text,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 import { ElementType } from "react";
 
 type NavLinkProps = ChakraLinkProps & {
     icon: ElementType;
     children: string;
     href: string;
+    matchExact?: boolean;
 };
-export function NavLink({ href, icon, children, ...rest }: NavLinkProps) {
+export function NavLink({
+    href,
+    icon,
+    children,
+    matchExact = false,
+    ...rest
+}: NavLinkProps) {
+    const { pathname } = useRouter();
+
+    let isActive = matchExact
+        ? [href, rest.as].includes(pathname)
+        : pathname.startsWith(href) || pathname.startsWith(String(rest.as));
+
     return (
         <ChakraLink
             as={NextLink}
             href={href}
             display="flex"
             alignItems="center"
+            color={isActive ? "pink.400" : "gray.50"}
             {...rest}
         >
             <Icon as={icon} fontSize="20" />
